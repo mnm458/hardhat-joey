8c93dc901750375298dd9c40754693bfc900acd4
Mohak Malhotra <mohakmalhotra@Mohaks-MacBook-Pro.local>
2023-07-21 20:30:37 +1000

    default userOp

--- src/buildUserOp.ts
+++ src/buildUserOp.ts
@@ -5,3 +5,17 @@ import { IUserOperation } from "./types";
 export const DEFAULT_VERIFICATION_GAS_LIMIT = 7000n;
 export const DEFAULT_CALL_GAS_LIMIT = 35000n;
 export const DEFAULT_PRE_VERIFICATION_GAS = 21000n;
+
+export const DEFAULT_USER_OP: IUserOperation = {
+    sender: ethers.ZeroAddress,
+    nonce: 0,
+    initCode: ethers.toBeHex("0x"),
+    callData: ethers.toBeHex("0x"),
+    callGasLimit: DEFAULT_CALL_GAS_LIMIT,
+    verificationGasLimit: DEFAULT_VERIFICATION_GAS_LIMIT,
+    preVerificationGas: DEFAULT_PRE_VERIFICATION_GAS,
+    maxFeePerGas: 0,
+    maxPriorityFeePerGas: 0,
+    paymasterAndData: ethers.toBeHex("0x"),
+    signature: ethers.toBeHex("0x"),
+  };
