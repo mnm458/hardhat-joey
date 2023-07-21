b6f7349cdbcae740a3078d8d1f8dbbd9d9c052ed
Mohak Malhotra <mohakmalhotra@Mohaks-MacBook-Pro.local>
2023-07-21 20:13:34 +1000

    structure set up

--- src/buildUserOp.ts
+++ src/buildUserOp.ts
@@ -1,1 +1,3 @@
-import { BigNumberish, BytesLike, ethers } from "ethers";
+export const DEFAULT_VERIFICATION_GAS_LIMIT = 7000n;
+export const DEFAULT_CALL_GAS_LIMIT = 35000n;
+export const DEFAULT_PRE_VERIFICATION_GAS = 21000n;
---
+++ src/types/IUserOperation.ts
@@ -0,0 +1,14 @@
+import { BigNumberish, BytesLike } from "ethers";
+export interface IUserOperation {
+    sender: string;
+    nonce: BigNumberish;
+    initCode: BytesLike;
+    callData: BytesLike;
+    callGasLimit: BigNumberish;
+    verificationGasLimit: BigNumberish;
+    preVerificationGas: BigNumberish;
+    maxFeePerGas: BigNumberish;
+    maxPriorityFeePerGas: BigNumberish;
+    paymasterAndData: BytesLike;
+    signature: BytesLike;
+  }
